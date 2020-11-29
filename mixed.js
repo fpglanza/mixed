62fbd9d1d7c289bcd05710b315b1c82585e0bc12
Filippo Lanza <fpg.lanza@gmail.com>
2020-11-29 12:53:56 +0100

    Added characters

--- mixed.js
+++ mixed.js
@@ -1,4 +1,7 @@
 // create a random phrase builder
+const cartoon = ['Kacchan', 'Killua', 'Gon', 'Goku'];
+const history = ['Genghis Khan', 'Julius Caesar', 'Napoleone', 'Ghandi'];
+const movie = ['Marty McFly', 'Heisenberg', 'Matt Murdock', 'Jay Kulina'];
 
 const wierdTrioBuilder = (cartoon, movie, history) => {
   let wierdTrio = []
